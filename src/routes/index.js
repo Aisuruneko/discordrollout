2274600013d9818bdb7e10f36362437309732ee8
Aisuruneko <officialtcgmatt@gmail.com>
2023-06-11 15:18:30 -0400

    v2023.06.11_2
    sadly, i must add a blocked endpoint because someone is using my data without permission...

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,9 +9,24 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
+const blocked = require("../../data/blocked.json");
+
 module.exports = (app) => {
 
 	const logger = new app.utils.logger(app, "SYSTEM");
+
+	app.use((req, res, next) => {
+		if (blocked[req.ip]) { // IP Blocked
+			const theBlocked = blocked[req.ip];
+			if (((typeof theBlocked.blocked === "object") ? req.url.includes(theBlocked.blocked) : theBlocked.blocked === "all")) {
+				const returningError = app.functions.returnError(403, `Blocked ${req.ip} from accessing ${req.url} due to ${theBlocked.reason}`, true);
+				returningError["message"] = theBlocked.blockMessage;
+				returningError["maintainer"] = theBlocked.containMaintainer ? { handle: process.env.MAINTAINER_HANDLE, platform: process.env.MAINTAINER_PLATFORM, url: process.env.MAINTAINER_URL } : null;
+				return res.status(returningError.code).json(returningError);
+			};
+		};
+		return next();
+	});
 
 	fs.readdirSync(path.join(__dirname, "/middlewares/")).filter(file => path.extname(file) === ".js").forEach(file => {
 		const router = require(path.join(__dirname, "/middlewares/", file));
