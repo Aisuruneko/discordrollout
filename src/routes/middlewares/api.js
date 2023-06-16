89ffe242bb7ce62b88d0f65a6abfe92814a005c1
Aisuruneko <officialtcgmatt@gmail.com>
2023-06-16 02:30:35 -0400

    v2023.06.16_hotfix3
    aha...ha...ha.haAAAAAAAAAAAAAAAA-

--- src/routes/middlewares/api.js
+++ src/routes/middlewares/api.js
@@ -188,7 +188,7 @@ module.exports = (app) => {
 
 		statusImgCache.lastChecked = currentTime;
 		try {
-			const browser = await puppeteer.launch({ headless: "new", args: [ "--no-sandbox", "--disable-setuid-sandbox" ] });
+			const browser = await puppeteer.launch({ headless: "true", args: [ "--no-sandbox", "--disable-setuid-sandbox" ] });
 			const page = await browser.newPage();
 
 			await page.setViewport({ width: 1200, height: DOMheight, deviceScaleFactor: 1 });
